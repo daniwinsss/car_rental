970d49f6bd7c0fbec92de8e4fa4ec12faff80962
daniwinsss <dankdani06@gmail.com>
2026-03-30 13:52:43 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
 })
