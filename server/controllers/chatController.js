0c4067e4855afa8ee320184b03f253356e0ee86b
daniwinsss <dankdani06@gmail.com>
2026-03-30 15:07:25 +0530

    commit

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -44,7 +44,7 @@ AVAILABLE CARS INVENTORY:
 ${inventorySummary}`;
 
     const model = genAI.getGenerativeModel({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash',
       systemInstruction,
     });
 
