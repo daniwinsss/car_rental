977b36af89d17e52f18effef29f65e9c49a3991b
daniwinsss <dankdani06@gmail.com>
2026-05-13 11:20:58 +0530

    c

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -56,57 +56,6 @@ export const AppProvider = ({children})=>{
     }
     //useEffect to retrieve token from localStorage
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-
-export const AppContext = createContext();
-
-export const AppProvider = ({children})=>{
-    const navigate = useNavigate();
-    const currency = import.meta.env.VITE_CURRENCY;
-    const [token,setToken] = useState(null);
-    const [user,setUser] = useState(null);
-    const [showLogin,setShowLogin] = useState(false);
-    const [isOwner,setIsOwner] = useState(false);
-    const[pickupDate,setPickupDate] = useState('')
-    const[returnDate,setReturnDate] = useState('')
-
-    const [cars,setCars] = useState([]);
-
-
-    //function to check user if logged in
-    const fetchUser = async()=>{
-        try {
-            const {data} = await axios.get('/api/user/data')
-            if(data.success){
-                setUser(data.user);
-                setIsOwner(data.user.role === 'owner');
-            }
-            else{
-                navigate('/')
-            }
-        } catch (error) {
-            toast.error(error.message);
-        }
-    }
-    //function to fetch all cars
-    const fetchCars = async()=>{
-        try {
-           const {data} = await axios.get('/api/user/cars')
-           data.success ? setCars(data.cars) : toast.error(data.message);
-        } catch (error) {   
-            toast.error(error.message);
-        }
-    }
-    //fn to logout user
-    const logout = ()=>{
-        localStorage.removeItem('token');
-        setUser(null);
-        setToken(null);
-        setIsOwner(false);
-        axios.defaults.headers.common['Authorization'] = '';
-        toast.success('you have been logged out')
-    }
-    //useEffect to retrieve token from localStorage
     useEffect(()=>{
         const token = localStorage.getItem('token');
         setToken(token);
